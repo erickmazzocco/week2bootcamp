69cec81ed7eb8cefb3bb06c47821c5e946733462
Érick Mazzocco <erick.mazzocco@elogroup.com.br>
2020-08-27 21:55:19 -0300

    upload files

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  userId: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ userId, avatarFileName }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne(userId);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+
+      const userAvatarFileExists = await fs.promises
+        .stat(userAvatarFilePath)
+        .catch(() => {
+          return null;
+        });
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
